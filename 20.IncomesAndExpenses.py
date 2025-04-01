d55b779a92c528310da25280a85b31268360dbff
Sergio Andres Santa Acevedo <sergiosanta970617@gmail.com>
2025-04-01 12:41:41 -0500

    New project started

---
+++ 20.IncomesAndExpenses.py
@@ -0,0 +1,18 @@
+#so this will be a first big projet to learn pytho
+import csv
+
+
+#register sergio incomes
+def incomesSergio ():
+    incomesSantaAutomotris = input('How much did you ern Santa Automotriz: ')
+    return incomesSantaAutomotris
+incomesSergio()
+#register Tatiana incomes
+def incomesTatiana ():
+    incomesRGprincipal = input('How much did you ern RG principal: ')
+    return incomesRGprincipal
+incomesTatiana():
+#register sergio incomes
+def register_any_expenses ():
+    
+    
