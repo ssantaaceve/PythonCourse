cb701a6f61ec803a5757ad50a3eb527c5c2fd579
Sergio Andres Santa Acevedo <sergiosanta970617@gmail.com>
2025-04-01 10:17:23 -0500

    Actualizacion aprendizaje, bucles

--- tablero.py
+++ tablero.py
@@ -1,34 +1,15 @@
-#Ejercicio 1
-#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
-
-palabra = input('type any word')
-
-for i in range(0,10):
-    print (palabra)
-
-#Ejercicio 2
-#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
-
-age_user = int(input('type your age'))
-
-for i in range (0,age_user):
-    print (i + 1)
+#Ejercicio 8
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo.
+#1
+#3 1
+#5 3 1
+#7 5 3 1
+#9 7 5 3 1
 
 
-#Ejercicio 3
-#Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+numero = int(input('Escribe un número entero positivo: '))
 
-numero_user = int(input('type any positive number '))
-for i in range (0, numero_user):
-    if i % 2 !=0:
-        print(f'{i}, ')
-
-
-#Ejercicio 4
-#Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la cuenta atrás desde ese número hasta cero separados por comas
-
-
-num = int(input('Escribe un número entero positivo: '))
-
-for i in range (num, -1, -1):
-    print(f'{i},')
+for i in range(1, numero + 1, 2):  # Inicia en 1 y se incrementa de 2 en 2
+    for j in range(i, 0, -2):  # Imprime números de i a 1 en decremento de 2
+        print(j, end=" ")  # Imprime los números en la misma línea
+    print()  # Salto de línea después de cada fila
