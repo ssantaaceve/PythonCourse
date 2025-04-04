a6c8dbcabc8b1ee72df4affc786692f97347f904
Sergio Andres Santa Acevedo <sergiosanta970617@gmail.com>
2025-04-04 15:41:14 -0500

    Actualizacion bucle 9

--- tablero.py
+++ tablero.py
@@ -1,15 +1,16 @@
-#Ejercicio 8
-#Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo.
-#1
-#3 1
-#5 3 1
-#7 5 3 1
-#9 7 5 3 1
+#Ejercicio 9
+#Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
 
 
-numero = int(input('Escribe un número entero positivo: '))
+user_password = "123456"
 
-for i in range(1, numero + 1, 2):  # Inicia en 1 y se incrementa de 2 en 2
-    for j in range(i, 0, -2):  # Imprime números de i a 1 en decremento de 2
-        print(j, end=" ")  # Imprime los números en la misma línea
-    print()  # Salto de línea después de cada fila
+def password_interfaz():
+    while True:
+        password = input("Type your password: ")
+        if password == user_password:
+            print("Access granted ✅")
+            break
+        else:
+            print("Try again ❌")
+
+password_interfaz()
